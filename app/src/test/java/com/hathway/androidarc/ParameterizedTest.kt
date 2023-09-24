c295e39cb2ebb7196ead7744682c787b6c147cf4
asifzafar00 <asizafar00@gmail.com>
2023-09-25 03:19:15 +0800

    xParameterized test way

---
+++ app/src/test/java/com/hathway/androidarc/ParameterizedTest.kt
@@ -0,0 +1,30 @@
+package com.hathway.androidarc
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(value = Parameterized::class)
+class ParameterizedTest( val input: String,  val expectedValue: Boolean) {
+
+    @Test
+    fun test() {
+        val helper = Myca()
+        val result = helper.isPallindrome(input)
+        assertEquals(expectedValue, result)
+    }
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name="{index} : {0} is Pallindrome - {1}")
+        fun data(): List<Array<Any>> {
+            return listOf(
+                arrayOf("hello", false),
+                arrayOf("level", true),
+                arrayOf("a", true),
+                arrayOf("", true)
+            )
+        }
+    }
+}
