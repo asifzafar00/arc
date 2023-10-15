175fed01588b8967527f130f4bc18f336cd28c98
asifzafar00 <asizafar00@gmail.com>
2023-10-15 13:00:35 +0800

    compose

--- app/src/main/java/com/hathway/androidarc/MainActivity.kt
+++ app/src/main/java/com/hathway/androidarc/MainActivity.kt
@@ -2,10 +2,18 @@ package com.hathway.androidarc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun BirthdayCardPreview() {
+
     }
 }
