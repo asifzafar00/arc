2853dd2c7991c8cd8c8c577770f86b09f93fd23e
asifzafar00 <asizafar00@gmail.com>
2023-10-21 16:52:47 +0800

    sample example of dagger @Module ,@Provider and @Binds

--- app/src/main/java/com/hathway/androidarc/UserRegistrationComponents.kt
+++ app/src/main/java/com/hathway/androidarc/UserRegistrationComponents.kt
@@ -14,9 +14,5 @@ interface UserRegistrationComponents {
 
 }
 
-// appComponent lives in the Application class to share its lifecycle
-class MyApplication: Application() {
-    // Reference to the application graph that is used across the whole app
-    //val appComponent = DaggerUserRegistrationComponents.create()
-}
+class MyApplication : Application()
 
--- app/src/main/java/com/hathway/androidarc/UserRepositoryModule.kt
+++ app/src/main/java/com/hathway/androidarc/UserRepositoryModule.kt
@@ -2,16 +2,9 @@ package com.hathway.androidarc
 
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 
 @Module
 abstract class UserRepositoryModule {
-
-   /* @Provides
-    fun getFireBaseUserRepository(fireBaseUserRepository: FireBaseUserRepository): UserRepository {
-        return FireBaseUserRepository()
-    }*/
-
     @Binds
     abstract fun getSQlUserRepository(sQlUserRepository: SQlUserRepository): UserRepository
 }
